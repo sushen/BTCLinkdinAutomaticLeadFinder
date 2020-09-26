8365a9dc6a10688f3815b9ccafcafd55ee8593d9
황지훈 <51396102+CodeZeehun@users.noreply.github.com>
2020-09-26 10:57:21 +0800

    Cleaned up Redundant Code in Connect Leads. Refactored message sent in ConnectLeads.py and SendMessage.py into send_message.py.

--- ManyChatLinkedInFinder/2.ConnectLeads.py
+++ ManyChatLinkedInFinder/2.ConnectLeads.py
@@ -15,6 +15,7 @@ import os
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from ManyChatLinkedInFinder import user_login
+from ManyChatLinkedInFinder import send_message
 
 # No 1 : Change
 # Message to send when connecting
@@ -39,11 +40,6 @@ url = "https://www.linkedin.com/sales/lists/people/6710357981692780544"
 driver.get(url)
 time.sleep(waiting_for_page)
 
-
-
-driver.get(url)
-time.sleep(waiting_for_page)
-
 try:
     pages = int(driver.find_element_by_class_name("search-results__pagination-list").find_elements_by_tag_name("li")[
                     -1].text.split("…")[-1])
@@ -51,62 +47,8 @@ except:
     pages = 1
 
 #TODO: Make a Massage Sending Function
-for i in range(pages):
 
-    people = driver.find_element_by_tag_name("table").find_elements_by_tag_name("tr")
-    people = people[1:]
-
-    aux_count = 0
-
-    for p in range(len(people)):
-
-        people = driver.find_element_by_tag_name("table").find_elements_by_tag_name("tr")
-        people = people[1:]
-
-        driver.execute_script("window.scrollTo(0, {})".format(aux_count))
-
-        time.sleep(1)
-
-        people[p].find_elements_by_tag_name("button")[-1].click()
-
-        time.sleep(2)
-
-        aux = people[p].find_element_by_class_name("artdeco-dropdown__content-inner").find_elements_by_tag_name("li")
-
-        for m in range(len(aux)):
-            # No 3 : Change
-            # Change to "Connect"
-            if "Connect" in aux[m].text:
-                aux[m].click()
-                time.sleep(1)
-
-                driver.find_element_by_id("connect-cta-form__invitation").send_keys(random.choice(message_to_connect))
-                time.sleep(2)
-
-                try:
-                    driver.find_element_by_id("connect-cta-form__email").send_keys(email)
-                    time.sleep(1)
-                except:
-                    pass
-
-                driver.find_element_by_class_name("connect-cta-form__send").click()
-
-                break
-
-            time.sleep(2)
-
-        driver.find_element_by_id("content-main").click()
-
-        aux_count += 80
-
-        # TODO: Fixed the pasination
-
-        try:
-            driver.find_element_by_class_name("artdeco-pagination__button").click()
-        except:
-            break
-
-        time.sleep(10)
+send_message.send_message_connect_leads(pages, driver, message_to_connect, email)
 
 
 # TODO: Test that in 3.2.ShuffleScriptSlowLinkdinUNDPUserList.py
